af52a3aa4076242438473faeae6272c9f373c98f
ani213 <vermaaniket213@gmail.com>
2022-01-21 11:25:03 +0530

    Operators

---
+++ python/Operators.py
@@ -0,0 +1,33 @@
+# Examples of Arithmetic Operator
+a = 9
+b = 4
+
+# Addition of numbers
+add = a + b
+
+# Subtraction of numbers
+sub = a - b
+
+# Multiplication of number
+mul = a * b
+
+# Division(float) of number
+div1 = a / b
+
+# Division(floor) of number
+div2 = a // b
+
+# Modulo of both number
+mod = a % b
+
+# Power
+p = a ** b
+
+# print results
+print(add,"Add")
+print(sub,"Substract")
+print(mul,"multiple")
+print(div1,"division")
+print(div2,"double divison")
+print(mod,"modulas")
+print(p,"power")
