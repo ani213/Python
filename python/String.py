3b3a194b65f8ad7797a8c80cff9d0bab8183e7b5
ani213 <vermaaniket213@gmail.com>
2022-02-23 09:22:34 +0530

    input and string

---
+++ python/String.py
@@ -0,0 +1,12 @@
+y=" Aniket "
+# for i in y:
+#     print(i)
+# print(len(y))
+# if("Ani" not in y):
+#     print("YES")
+# elif("Ani" in y):
+#     print("Yes occure")    
+# else:
+#     print("No")    
+# print(y[-1:])
+print(y.strip())
